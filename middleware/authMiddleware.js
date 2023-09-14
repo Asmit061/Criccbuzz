4ca30aee8e0bc6dede9d4ba9b47436c34c076cd7
Asmit <asmitbuddy@gmail.com>
2023-09-14 12:46:57 +0530

    another bug fixed -v3

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,21 +1,20 @@
-
-
 const jwt = require("jsonwebtoken");
 const config = require("../config");
 
 function authenticateUser(req, res, next) {
-    const token = req.header("Authorization");
-    if (token) {
-      jwt.verify(token, config.secretKey, (err, decoded) => {
-        if (err) {
-          return res.status(401).json({ message: "Invalid token" });
-        }
-        req.user = decoded;
-        next();
-      });
-    } else {
-       return res.status(401).json({ message: "Unauthorized" });
-    }
+  console.log(req.get("header"));
+  const token = req.header("Authorization");
+  if (token) {
+    jwt.verify(token, config.secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: "Invalid token" });
+      }
+      req.user = decoded;
+      next();
+    });
+  } else {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 }
 
 module.exports = {
